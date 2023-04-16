ed5a55367dd74b767c553962273c7ad1998025a2
gabhishekrao <119596349+gabhishekrao@users.noreply.github.com>
2023-04-17 00:19:45 +0530

    first commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const PORT = process.env.PORT || 8800
+const PORT = 8800
 
 
 const app = express();
