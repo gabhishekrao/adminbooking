a6753d56dce12b2028a07772d5c07a329ebb8c7d
gabhishekrao <119596349+gabhishekrao@users.noreply.github.com>
2023-04-17 00:13:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import  path  from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const PORT = process.env.PORT || 8800
+
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.set("strictQuery", true);
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
+
+//middlewares
+app.use(cors())
+app.use(cookieParser())
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+// static files
+
+app.use(express.static(path.join(__dirname, "./admin/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./admin/build/index.html"));
+});
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
+
+app.listen(PORT, () => {
+  connect();
+  console.log("Connected to backend.");
+});
